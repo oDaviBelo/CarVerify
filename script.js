9701c75ae73ae2275bac28e747092e2c135c6ce2
Davi Belo <davibelocontato@gmail.com>
2022-05-25 22:13:28 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var carro = 'Cruze';
+var MarcaCarro = '';
+
+switch(carro) {
+    case 'Cruze':
+        MarcaCarro = 'Chevrolet';
+        break
+    case 'Hilux':
+        MarcaCarro = 'Toyota';
+        break
+    case 'Civic':
+        MarcaCarro = 'Honda';
+        break   
+    case 'Nivus':
+        MarcaCarro = 'Volkswagen';
+        break
+    case 'Uno':
+        MarcaCarro = 'Fiat';
+        break         
+}
+
+console.log('O' + ' ' + carro + ' é da' + ' ' + MarcaCarro)
